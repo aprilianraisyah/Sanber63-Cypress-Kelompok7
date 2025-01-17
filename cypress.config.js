62a976fdfddae6bfbc04bf015136a2ec066fe6f2
Habib Akbar Wibowo <hbibakber@gmail.com>
2025-01-17 21:53:25 +0700

    build cypress project

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/',
+    setupNodeEvents(on, config) {
+      
+    },
+  },
+});
