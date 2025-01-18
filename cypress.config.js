b3548d2594e2635c1ac8800162e912ba73551e46
Ilma Nurdini <ilmanurdinimr@gmail.com>
2025-01-18 23:47:37 +0700

    Menambahkan script test untuk fitur Edit Account

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,28 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs');
+const path = require('path');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/',
+    baseUrl: 'https://magento.softwaretestingboard.com/customer/account/login/',
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
-      
+      on('task', {
+        updateEmailInUsersJson({ randomEmail, randomPassword }) {
+            const filePath = path.resolve('cypress/fixtures/myAccount/users.json'); // Path to your JSON file
+            const users = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+            // Update email in the JSON file
+            users[0].email = randomEmail;
+            users[0].password = randomPassword; // Update password if necessary
+
+            // Write the updated content back to the file
+            fs.writeFileSync(filePath, JSON.stringify(users, null, 2), 'utf8');
+            return null; // Task successful
+        }
+    });
     },
   },
+  viewportWidth: 1280,
+  viewportHeight: 720,
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,7 +25,39 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 // Login
-Cypress.Commands.add('login', (email, password) => {
+Cypress.Commands.add('login', (email, password, loginBtn) => {
     cy.get('#email').type(email)
     cy.get('#pass').type(password)
+    cy.get('#send2').click()
+})
+
+Cypress.Commands.add('klik', (locator) => {
+    cy.get(locator).should('be.visible').click()
+})
+
+Cypress.Commands.add('input', (locator, value) => {
+    cy.get(locator)
+        .should('be.visible')
+        .clear()
+        .type(value)
+})
+
+Cypress.Commands.add('verifyContainText', (locator, value) => {
+    cy.get(locator)
+        .should('be.visible')
+        .should('contain.text', value)
+})
+
+Cypress.Commands.add('verifyErrorState', (fieldLocator, errorTextLocator, errorText) => {
+    cy.get(fieldLocator).should('have.css', 'border-color', 'rgb(237, 131, 128)')
+    cy.get(errorTextLocator).should('be.visible').should('contain', errorText)
+})
+
+Cypress.Commands.add('verifySnackbar', (snackbarLocator, snackbarTextLocator, text) => {
+    cy.get(snackbarLocator).should('be.visible')
+    cy.verifyContainText(snackbarTextLocator, text)
+})
+
+Cypress.Commands.add('verifyElementIsVisible', (locator) => {
+    cy.get(locator).should('be.visible')
 })
