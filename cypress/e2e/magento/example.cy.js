68190076f45e946eb2ffbb21f25cd6cd1f02d3e6
Ilma Nurdini <ilmanurdinimr@gmail.com>
2025-01-18 23:51:12 +0700

    Update example.cy.js

--- cypress/e2e/magento/example.cy.js
+++ cypress/e2e/magento/example.cy.js
@@ -1,24 +1,5 @@
-import pageObject from "../../support/myAccount/pageObject"
-import users from '../../fixtures/myAccount/users.json'
-import { faker } from '@faker-js/faker'
-
-const PageObject = new pageObject()
-const user = users[0]
-const firstName = faker.name.firstName()
-const lastName = faker.name.lastName()
-const randomEmail = faker.internet.email();
-const randomPassword = faker.internet.password(12, true);
-
-describe('Edit Account Information', () => {
-  it('Edit First Name', () => {
-      //Login
-      cy.visit('/')
-      cy.login(user.email, user.password)
-      cy.verifyContainText(PageObject.signedInUser, "Welcome")
-
-      //Buka My Account 
-      cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
-      cy.get(':nth-child(2) > .customer-welcome > .customer-menu > .header > :nth-child(1) > a').click()
-      cy.verifyContainText('.base', "My Account")
-
-  })})
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+  })
+})
