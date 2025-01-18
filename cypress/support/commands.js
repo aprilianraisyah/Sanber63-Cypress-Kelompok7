9236aafbdd3f21877429c8f2dc4d01d66cb551c3
ilmanurdinii <ilmanurdinimr@gmail.com>
2025-01-19 00:22:15 +0700

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,3 +30,47 @@ Cypress.Commands.add('login', (email, password) => {
     cy.get('#pass').type(password)
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
 })
+
+Cypress.Commands.add('inputText', (locator, text) => {
+    cy.get(locator)
+    .should('be.visible')
+    .clear()
+    .type(text)
+})
+
+Cypress.Commands.add('userLogin', (email, password, loginBtn) => {
+    cy.get('#email').type(email)
+    cy.get('#pass').type(password)
+    cy.get('#send2').click()
+})
+
+Cypress.Commands.add('klik', (locator) => {
+    cy.get(locator).should('be.visible').click()
+})
+
+Cypress.Commands.add('input', (locator, value) => {
+    cy.get(locator)
+        .should('be.visible')
+        .clear()
+        .type(value)
+})
+
+Cypress.Commands.add('verifyContainText', (locator, value) => {
+    cy.get(locator)
+        .should('be.visible')
+        .should('contain.text', value)
+})
+
+Cypress.Commands.add('verifyErrorState', (fieldLocator, errorTextLocator, errorText) => {
+    cy.get(fieldLocator).should('have.css', 'border-color', 'rgb(237, 131, 128)')
+    cy.get(errorTextLocator).should('be.visible').should('contain', errorText)
+})
+
+Cypress.Commands.add('verifySnackbar', (snackbarLocator, snackbarTextLocator, text) => {
+    cy.get(snackbarLocator).should('be.visible')
+    cy.verifyContainText(snackbarTextLocator, text)
+})
+
+Cypress.Commands.add('verifyElementIsVisible', (locator) => {
+    cy.get(locator).should('be.visible')
+})
